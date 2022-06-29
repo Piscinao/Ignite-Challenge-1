1d0b98ec440d452d2be0be8f49b087306339d7d4
Piscinao <henhvj@gmail.com>
2022-06-29 16:22:57 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,59 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  // complete aqui
+  const { username } = request.headers;
+
+  // procura por username
+  const user = users.find((user) => user.username === username);
+  
+  //Não deve ser possivel buscar com username inexistente
+  if(!user){
+    return response.status(400).json({error: "User not found"});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+
+  const { name, username } = request.body;
+  
+
+  const userAlreadyExistis = users.some(
+    (user) => user.username === username
+  );
+
+  if(userAlreadyExistis){
+    return response.status(400).json({error: "User already exists!"});
+  }
+
+  //chamada de função uuidv4
+  // const id = uuidv4();
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -24,6 +62,21 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const userTodo = {
+    id: uuidv4(), 
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(userTodo);
+
+  return response.status(201).send();
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
